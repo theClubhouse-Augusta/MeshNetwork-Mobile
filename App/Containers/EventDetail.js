1c90bca57a63cace4b2e5245e81f45cc5bdf5bb4
Audora Chalker <scribeklio@gmail.com>
2017-10-20 14:21:55 -0400

    event detail pg tentative

--- App/Containers/EventDetail.js
+++ App/Containers/EventDetail.js
@@ -14,22 +14,22 @@ export default class EventDetail extends React.Component {
 
     return (
       <ScrollView>
-          <View style={styles.eventImage}></View>
+          <Image style={styles.eventImage} source={require('./Images/placeholder.jpg')}>
+            <View > 
+            <Text style={styles.eventName}>PyNight</Text>
+            </View>
+          </Image>
           <View style={styles.eventDetails}>
             <View> 
-                <Text> the Clubhou.se</Text>
+                <Text style={styles.detailText}> the Clubhou.se</Text>
             </View>
             <View> 
-                <Text> 03/15/18</Text>
+                <Text style={styles.detailText}> 03/15/18</Text>
             </View>
             <View> 
-                <Text> 6-8pm</Text>
+                <Text style={styles.detailText}> 6-8pm</Text>
             </View>
-          </View>
-
-          <Button title='register' onPress={NavigationActions.Root} style={styles.eventRegister}> </Button>
-
-          <View style={styles.eventDescription}></View>   
+          </View>          
 
           <View style={styles.eventTagRow}> 
             <View>
@@ -41,6 +41,13 @@ export default class EventDetail extends React.Component {
             <View>
               <Text style={styles.eventTag}> Development </Text>
             </View>
+          </View>
+
+    
+          <Text style={styles.eventDescription}>Cat ipsum dolor sit amet, cat fur is the new black for kitty kitty wake up wander around the house making large amounts of noise jump on top of your human's bed and fall asleep again. </Text>  
+
+          <View style={styles.eventRegister}> 
+          <Button title='register' onPress={NavigationActions.Root}  accessibilityLabel="Register for event"> </Button>
           </View>
 
       </ScrollView>
--- App/Containers/Styles/EventStyle.js
+++ App/Containers/Styles/EventStyle.js
@@ -11,28 +11,45 @@ export default StyleSheet.create({
   },
 
   eventImage: {
-    height: 200, 
+    height: 200,
+    width: width,  
     backgroundColor: '#BBBBBB',
+     
+    
+  }, 
+
+  eventName: { 
+    fontSize: 32,
+    color: '#ffffff',
+    marginTop: 120, 
+    marginLeft: 10,  
   }, 
 
   eventDetails: {
-    height: 80, 
+    height: 60, 
     backgroundColor: '#ffffff', 
-    marginBottom: 30, 
+    marginBottom: 30,
+    paddingRight: 10,
+    paddingLeft: 5,
+    paddingTop: 20, 
+    display: 'flex', 
+    flexDirection: 'row',   
+    justifyContent: 'space-between', 
+
   }, 
 
-  eventRegister: {
-
+  detailText: {
+    fontSize: 18, 
   }, 
 
   eventDescription: {
     marginTop: 30, 
     height: 150, 
+    padding: 20, 
     backgroundColor: '#CCCCCC', 
   }, 
 
-  eventTagRow: { 
-    marginTop: 30, 
+  eventTagRow: {
     marginLeft: 5, 
     marginRight: 5, 
     display: 'flex', 
@@ -46,5 +63,12 @@ export default StyleSheet.create({
     padding: 10, 
     borderWidth: 1, 
     borderColor: '#000', 
+    backgroundColor: '#ffbe39', 
+  }, 
+
+  eventRegister: { 
+    width: width - 40, 
+    alignSelf: 'center', 
+    marginTop: 10, 
   }, 
 }) 
